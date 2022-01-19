63aeb50f7ab25b8dec8d4d547511a509df3a939b
Antonio Pisano <antonio@elettrotecnica.it>
2022-01-19 17:17:50 +0000

    Cleanup redundant definition

--- tcl/test/test-item-procs.tcl
+++ tcl/test/test-item-procs.tcl
@@ -416,21 +416,6 @@ namespace eval ::xowf::test {
         }
     }
 
-    aa_register_init_class \
-        xowf_require_test_instance {
-            Make sure the test instance is there and create it if necessary.
-        } {
-            aa_export_vars {_xowf_test_instance_name}
-            set _xowf_test_instance_name /xowf-test
-            ::acs::test::require_package_instance \
-                -package_key xowf \
-                -instance_name $_xowf_test_instance_name
-        } {
-            # Here one might unmount the package afterwards. Right now
-            # we decide to keep it so it is possible to e.g. inspect
-            # the results or test further in the mounted instance.
-        }
-
     aa_register_case -init_classes {xowf_require_test_instance} -cats {web} -procs {
         "::lang::system::locale"
         "::xowiki::test::create_form_page"
