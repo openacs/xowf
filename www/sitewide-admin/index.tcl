cbb3bc842fdfd0ad1f7eb5d6b842fb6bea78350f
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-02-02 12:07:02 +0000

    Added site-wide admin pages for xowf
    This makes it easy for admins to locate the site-wide pages
    (pages, which can be used in all xowf instances)

---
+++ www/sitewide-admin/index.tcl
@@ -0,0 +1,17 @@
+ad_page_contract {
+    @author Gustaf Neumann
+
+    @creation-date July, 2020
+} {
+}
+set title "[::xowf::Package pretty_name] - Site-wide Admin"
+set context [list $title]
+
+set package_key [::xowf::Package package_key]
+
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
