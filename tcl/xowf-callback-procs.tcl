a7f9aa48ca208a4e4beb080e978789b216a2c37e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-08-29 17:53:50 +0000

    make sure to provide a valid user_id when running in the after-initialize callback

--- tcl/xowf-callback-procs.tcl
+++ tcl/xowf-callback-procs.tcl
@@ -16,7 +16,21 @@ namespace eval ::xowf {
     #
     # Create a parameter page for convenience
     #
-    ::xowf::Package initialize -package_id $package_id
+    # The parameter page needs a creation user. Since we are running
+    # in a callback, the user_id is 0, which is not defined in the
+    # users table. Therefore, we fetch the first site-wide admin user.
+    #
+    set user_id [::xo::dc list get_admin {
+      select user_id,p.object_id from acs_permissions p, users u, acs_magic_objects m
+      where user_id = p.grantee_id and p.object_id = m.object_id and m.name = 'security_context_root'
+      FETCH FIRST 1 ROWS ONLY
+    }]
+    #
+    # Initialize the package
+    #
+    ns_log notice ".... ::xowf::after-instantiate initialize package with -package_id $package_id -user_id $user_id"
+    ::xowf::Package initialize -package_id $package_id -user_id $user_id
+
     ::xowf::Package configure_fresh_instance \
         -package_id $package_id \
         -parameters [::xowf::Package default_package_parameters] \
