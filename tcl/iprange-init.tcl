220b2add24015277617604a958cac2b4d372645a
Antonio Pisano <antonio@elettrotecnica.it>
2024-11-06 15:51:59 +0000

    Bring existing doc into the object's documentation, so that tests would not fail...

--- tcl/iprange-init.tcl
+++ tcl/iprange-init.tcl
@@ -3,22 +3,26 @@
 #
 namespace eval ::xowf::iprange {
 
-    #
-    # The "all" range should be available on every sites
-    #
     ::xowf::IpRange create ::xowf::iprange::all \
         -title "All" \
-        -allowed {*}
-
-    #
-    # One example for a WLAN. Note that multiple specs for allowed and
-    # disalled can be specified. For the allowed formats, see:
-    #
-    #    https://openacs.org/xotcl/show-object?object=::xowf::IpRange
-    #
-    # ::xowf::IpRange create ::xowf::iprange::wlan \
-    #    -title "WU WLAN" \
-    #    -allowed {
-    #        137.208.216.0/21
-    #    }
+        -allowed {*} {
+            #
+            # The "all" range should be available on every site.
+            #
+            # One example for a WLAN. Note that multiple specs for allowed and
+            # disalled can be specified. For the allowed formats, see:
+            #
+            #    https://openacs.org/xotcl/show-object?object=::xowf::IpRange
+            #
+            # ::xowf::IpRange create ::xowf::iprange::wlan \
+            #    -title "WU WLAN" \
+            #    -allowed {
+            #        137.208.216.0/21
+            #    } {
+            #       #
+            #       # I am the special WU IP range.
+            #       #
+            #    }
+            #
+        }
 }
